590eec4e64b28dd11c92452c88336e6deca7978f
Chibukasia Nelson <chibukasianelson@gmail.com>
2023-01-29 11:42:39 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -18,10 +18,18 @@ function App() {
           <li className="pr-5">
             <a href="">Home</a>
           </li>
-          <li className="pr-5 over:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300"><a href="#about">About</a></li>
-          <li className="pr-5 over:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300"><a href="#projects">Projects</a></li>
-          <li className="pr-5 over:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300"><a href="#contact">Contact Me</a></li>
-          <li className="pr-0 over:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300"><a href="#">Resume</a></li>
+          <li className="pr-5 over:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300">
+            <a href="#about">About</a>
+          </li>
+          <li className="pr-5 over:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300">
+            <a href="#projects">Projects</a>
+          </li>
+          <li className="pr-5 over:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300">
+            <a href="#contact">Contact Me</a>
+          </li>
+          <li className="pr-0 over:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300">
+            <a href="#">Resume</a>
+          </li>
         </ul>
 
         <h2 className="text-2xl">
@@ -31,18 +39,26 @@ function App() {
       <section className="px-10 rounded-lg" id="home">
         <div className="text-center p-10 pt-2">
           <h2 className="text-5xl py-2 text-indigo-500 font-medium">
-             { `Dev {Chibukasia}`}
+            {`Dev {Chibukasia}`}
           </h2>
-          <h3 className="text-3xl py-2 text-center text-indigo-500 font-sans">Hi, I'm Nelson Chibukasia</h3>
-          <h3 className="text-3xl py-2 font-sans">FullStack Software Developers</h3>
+          <h3 className="text-3xl py-2 text-center text-indigo-500 font-sans">
+            Hi, I'm Nelson Chibukasia
+          </h3>
+          <h3 className="text-3xl py-2 font-sans">
+            FullStack Software Developers
+          </h3>
         </div>
-        
-        <div className="relative mx-auto bg-indigo-900 rounded-full w-80 h-80 mb-20 overflow-hidden hover:animate-spin">
-          <img className="object-cover" src={profile} layout="fill" />
+
+        <div className="relative mx-auto bg-indigo-900 rounded-full w-80 h-80 mb-20 overflow-hidden hover:animate-spin ">
+          <img
+            className="object-cover float-left"
+            src={profile}
+            layout="fill"
+          />
         </div>
-        
+
         <hr />
-        
+
         <hr />
         <div className="bg-slate-900 dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
           <div className="text-center pt-10  text-indigo-500 font-medium">
@@ -50,10 +66,10 @@ function App() {
           </div>
           <div className="text-5xl flex flex-wrap justify-center gap-16 py-3">
             <a className=" text-orange-600">
-              <SiCss3/>
+              <SiCss3 />
             </a>
             <a className=" text-orange-600">
-              <FaHtml5/>
+              <FaHtml5 />
             </a>
             <a className=" text-amber-500">
               <TbBrandJavascript />
@@ -79,17 +95,16 @@ function App() {
       <hr />
       <section className="px-10">
         <div id="about">
-        <About />
+          <About />
         </div>
         <hr />
         <div id="projects">
-        <Projects />
+          <Projects />
         </div>
         <hr />
         <div id="contact">
-        <Contact />
+          <Contact />
         </div>
-        
       </section>
     </div>
   );
